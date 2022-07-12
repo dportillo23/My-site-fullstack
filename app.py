94c72ccd80095929e2996360387298f88e3a8ed0
Daniel Portillo <depr.daniel@gmail.com>
2022-07-12 12:36:53 +0200

    add runtime.txt

--- app.py
+++ app.py
@@ -2,6 +2,9 @@ from flask import Flask
 from views import views
 import os
 from flask_wtf.csrf import CSRFProtect
+from dotenv import load_dotenv
+
+load_dotenv()
 
 app = Flask(__name__)
 csrf = CSRFProtect(app)
@@ -10,6 +13,7 @@ SECRET_KEY = os.urandom(32)
 app.config['SECRET_KEY'] = SECRET_KEY
 app.register_blueprint(views, url_prefix="/")
 
+DEBUG_APP = (True if os.getenv('DEBUG') == "True" else False)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=DEBUG_APP)
